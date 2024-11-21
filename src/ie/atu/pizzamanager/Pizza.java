0f66822c4acf84db8e5853692ef7929d338a18a0
Waseygmit1 <145453995+Waseygmit1@users.noreply.github.com>
2024-11-21 16:27:56 +0000

    Second Layout

--- src/ie/atu/pizzamanager/Pizza.java
+++ src/ie/atu/pizzamanager/Pizza.java
@@ -2,4 +2,51 @@ package ie.atu.pizzamanager;
 
 public class Pizza {
 
+    // Instance Variables
+    private int pizzaId; // Unique ID
+    private String toppings;
+    private float price;
+    private boolean isVegan;
+
+    // Constructor
+    public Pizza(int pizzaId , String toppings , float price , boolean isVegan){
+        this.pizzaId = pizzaId;
+        this.toppings = toppings;
+        this.price = price;
+        this.isVegan = isVegan;
+    }
+
+    //Getter and Setter Methods
+    public int getPizzaId(){
+        return this.pizzaId;
+    }
+
+
+    public void setPizzaId(int pizzaId){
+        this.pizzaId = pizzaId;
+    }
+
+    public String getToppings(){
+        return this.toppings;
+    }
+
+    public void setToppings(String toppings){
+        this.toppings = toppings;
+    }
+     
+    public float getPrice(){
+        return this.price;
+    }
+
+    public void setPrice(float price){
+        this.price = price;
+    }
+
+    public boolean getIsVegan(){
+        return this.isVegan;
+    }
+
+    public void setIsVegan(boolean isVegan){
+        this.isVegan = isVegan;
+    }
 }
